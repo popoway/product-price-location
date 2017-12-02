d220837ce48d2298d7a1eea95d3390800f83c1a6
popoway <31576265+popoway@users.noreply.github.com>
2017-12-02 15:22:06 -0500

    style container

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,7 @@
 function initMap() {
   var uluru = {lat: 40.736234, lng: -73.816079};
   var map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 4,
+    zoom: 15,
     center: uluru
   });
   var marker = new google.maps.Marker({
